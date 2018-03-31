15efb293b81a89410d9581433fe394036d402c1d
QpicMachi <33619867+QpicMachi@users.noreply.github.com>
2018-03-31 11:41:12 +0900

    2階層途中

--- Assets/scripts/Controller/SwitchController.cs
+++ Assets/scripts/Controller/SwitchController.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -56,3 +57,63 @@ public class SwitchController : MonoBehaviour {
         }
     }
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour {
+
+    public LiftController[] Lift;
+	public GameObject arrow;
+
+    private int LiftNum;
+    // Use this for initialization
+    void Start () {
+        LiftNum = Lift.GetLength(0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void TurnSwitch()
+    {
+        for(int i = 0; i < LiftNum; i++)
+        {
+            if (Lift[i].LiftFlag == false)
+            {
+                Lift[i].LiftFlag = true;
+
+				var heading = Lift [i].transform.position - this.transform.position;
+				var distance = heading.magnitude;
+				var direction = heading / distance;
+				var CurrentDirection = direction * 2;
+
+				var diff = (Lift[i].transform.position - transform.position ).normalized;
+				var rote = Quaternion.FromToRotation( Vector3.up,  diff);
+
+				Instantiate (arrow, CurrentDirection + this.transform.position, rote);
+
+
+            }
+            else
+            {
+                Lift[i].LiftFlag = false;
+
+				var heading = Lift [i].transform.position - this.transform.position;
+				var distance = heading.magnitude;
+				var direction = heading / distance;
+				var CurrentDirection = direction * 2;
+
+				var diff = (Lift[i].transform.position - transform.position ).normalized;
+				var rote = Quaternion.FromToRotation( Vector3.up,  diff);
+
+				Instantiate (arrow, CurrentDirection + this.transform.position, rote);
+
+
+            }
+        }
+    }
+}
+>>>>>>> origin/master
